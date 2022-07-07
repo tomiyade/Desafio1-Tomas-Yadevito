4a7dc2c0756af264a5157edc02d15562aabcf9d7
Tomas Yadevito <yadevitotomas@gmail.com>
2022-07-07 19:20:55 -0300

    esta es

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ do{
     let nota1 = parseFloat(prompt("Ingrese la primera nota"))
     let nota2 = parseFloat(prompt("Ingrese la segunda nota"))
     let nota3 = parseFloat(prompt("Ingrese la tercer nota"))
-    //let promedio = (nota1 + nota2 + nota3)/3
+    let promedio = (nota1 + nota2 + nota3)/3
 
     if(isNaN(nota1) || isNaN(nota2) || isNaN(nota3)){
         alert("Ingrese una nota valida")
